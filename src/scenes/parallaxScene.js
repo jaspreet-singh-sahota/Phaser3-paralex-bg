566e550509f192f13e1ee55f91feb9da6c9803e4
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-14 10:27:52 +0530

    Add sky image

--- src/scenes/parallaxScene.js
+++ src/scenes/parallaxScene.js
@@ -1,1 +1,18 @@
-console.log('hello')
+export default class ParallaxScene extends Phaser.Scene {
+  constructor() {
+    super('parallax-scene')
+  }
+
+  preload() {
+    this.load.image('sky', 'assets/images/sky.png');
+
+  }
+
+  create() {
+    const height = this.scale.height
+    const width = this.scale.width
+    
+    this.add.image(width * 0.5, height * 0.5 , 'sky')
+  }
+
+}

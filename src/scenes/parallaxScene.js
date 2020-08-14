57da10688ab484347af3959481b508711ba4e85a
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-15 04:55:27 +0530

    Add physics to player and ground

--- src/scenes/parallaxScene.js
+++ src/scenes/parallaxScene.js
@@ -1,10 +1,14 @@
-const backgroundRepeat = (scene, w, h, text, speed, s1, s2, o1, o2) => {
+const backgroundRepeat = (scene, w, h, text, speed, s1, s2, o1, o2, player) => {
   const count = 101 * speed
   let screenWidth = 0;
+  const platforms = scene.physics.add.staticGroup();
   
   for (let i = 0; i < count; i++) {
-    scene.add.image((w + screenWidth), h, text).setOrigin( o1, o2).setScrollFactor(speed).setScale(s1, s2)
+    const repeatImage = platforms.create((w + screenWidth), h, text).setOrigin( o1, o2).setScrollFactor(speed).setScale(s1, s2)
     screenWidth += scene.scale.width
+    if (text === "ground2") {
+      scene.physics.add.collider(player, repeatImage);
+    }
   }
 }
 
@@ -12,6 +16,11 @@ export default class ParallaxScene extends Phaser.Scene {
   constructor() {
     super('parallax-scene')
   }
+
+  init() {
+    this.playerSpeed = 150;
+    this.jumpSpeed = -600;
+  };
 
   preload() {
     this.load.image('sky', 'assets/images/sky.png');
@@ -31,6 +40,12 @@ export default class ParallaxScene extends Phaser.Scene {
     this.load.image('flower2', 'assets/images/flower2.png');
     this.load.image('ground2', 'assets/images/ground2.png');
 
+    this.load.spritesheet('player', 'assets/images/player.png', {
+      frameWidth: 50.5,
+      frameHeight: 52,
+      margin: 2,
+      spacing: 2,
+    });
   }
 
   create() {
@@ -40,7 +55,7 @@ export default class ParallaxScene extends Phaser.Scene {
     this.add.image(width * 0.5, height * 0.3 , 'sky')
     .setScrollFactor(0).setScale(0.8, 0.7)
 
-    this.cloud1 = backgroundRepeat(this, 0, height * 0.45,'cloud1', 0.15, 0.5, 0.5, 0, 1)
+    this.cloud1 = backgroundRepeat(this, 0, height * 0.45,'cloud1', 0.07, 0.5, 0.5, 0, 1)
     this.mountain = backgroundRepeat(this, 0, height,'mountain', 0.25, 0.5, 0.5, 0, 1)
     this.cloud2 = backgroundRepeat(this, width/ 2, height * 0.5,'cloud2', 0.15, 0.5, 0.5, 0, 1)
     this.grass2 = backgroundRepeat(this, width / 2.4, height / 1.5,'grass2', 0.5, 0.4, 0.4) 
@@ -53,9 +68,30 @@ export default class ParallaxScene extends Phaser.Scene {
     this.rock1 = backgroundRepeat(this, width / 1.8, height / 1.3,'rock2', 0.75, 0.4, 0.4) 
     this.rock2 = backgroundRepeat(this, width / 3.5, height / 1.3,'rock3', 0.75, 0.4, 0.4) 
     this.rock3 = backgroundRepeat(this, width / 1.1, height / 1.3,'rock1', 0.75, 0.4, 0.4) 
-    this.flower1 = backgroundRepeat(this, width / 1.7, height / 1.2,'flower1', 0.75, 0.4, 0.4) 
+    this.flower1 = backgroundRepeat(this, width / 1.7, height / 1.2,'flower1', 0.75, 0.4, 0.4)
+
+    this.player = this.add.sprite(1000 * 0.1, 700 * 0.45, 'player', 3).setScale(1.5, 1.5)
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds(true);
+    this.cameras.main.startFollow(this.player);
+    
+    
     this.flower2 = backgroundRepeat(this, width / 2.5, height / 1.3,'flower2', 0.75, 0.4, 0.4) 
-    this.ground2 = backgroundRepeat(this, 0, height,'ground2', 1.25, 0.45, 0.45, 0, 1) 
+    this.ground2 = backgroundRepeat(this, 0, height,'ground2', 1.25, 0.45, 0.45, 0, 1 , this.player)
+    // this.physics.add.overlap(this.player, [this.ground2], null, this);
+
+    if (!this.anims.get('walking')) {
+      // walking animation
+      this.anims.create({
+        key: 'walking',
+        frames: this.anims.generateFrameNames('player', {
+          frames: [30, 31, 32, 33, 34, 35]
+        }),
+        frameRate: 12,
+        yoyo: true,
+        repeat: -1
+      });
+    }
     
     this.cursors = this.input.keyboard.createCursorKeys();
     this.cameras.main.setBounds(0,0, width * 100 ,height)
@@ -65,12 +101,59 @@ export default class ParallaxScene extends Phaser.Scene {
     const speed = 10
     const cam = this.cameras.main
 
+
     if (this.cursors.left.isDown) {
+      console.log(this.player)
+      this.player.setVelocityX(130);
       cam.scrollX -= speed
     }
     else if (this.cursors.right.isDown) {
       cam.scrollX += speed
+
     }
+
+    // // movement to the left
+    // if (this.cursors.left.isDown) {
+
+    //   this.player.flipX = true;
+
+    //   // play animation if none is playing
+    //   if (onGround && !this.player.anims.isPlaying)
+    //     this.player.anims.play('walking');
+    // }
+
+    // // movement to the right
+    // else if (this.cursors.right.isDown) {
+    //   this.player.setVelocityX(this.playerSpeed);
+// 
+    //   this.player.flipX = false;
+
+    //   // play animation if none is playing
+    //   if (onGround && !this.player.anims.isPlaying)
+    //     this.player.anims.play('walking');
+    // } else {
+    //   // make the player stop
+    //   this.player.body.setVelocityX(0);
+
+    //   // stop walking animation
+    //   this.player.anims.stop('walking');
+
+    //   // set default frame
+    //   if (onGround)
+    //     this.player.setFrame(10);
+    // }
+
+    // // handle jumping
+    // if (onGround && (this.cursors.space.isDown || this.cursors.up.isDown)) {
+    //   // give the player a velocity in Y
+    //   this.player.body.setVelocityY(this.jumpSpeed);
+
+    //   // stop the walking animation
+    //   this.player.anims.stop('walking');
+
+    //   // change frame
+    //   this.player.setFrame(42);
+    // }
   }
   
 }

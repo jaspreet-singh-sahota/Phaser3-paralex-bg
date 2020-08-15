965593165c67dba5a2d9c977c0bc0ba694872e13
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-16 04:01:19 +0530

    Add scoring card

--- src/scenes/parallaxScene.js
+++ src/scenes/parallaxScene.js
@@ -1,18 +1,13 @@
-const backgroundRepeat = (scene, w, h, text, speed, s1, s2, o1, o2, player) => {
-  const count = 101 * speed
-  let screenWidth = 0;
-  const platforms = scene.physics.add.staticGroup();
-  
-  for (let i = 0; i < count; i++) {
-    const repeatImage = platforms.create((w + screenWidth), h, text).setOrigin( o1, o2).setScrollFactor(speed).setScale(s1, s2)
-    screenWidth += scene.scale.width
-    if (text === "ground2" || text === "star") {
-      scene.physics.add.collider(player, repeatImage);
-      if (text === 'star') {
-        
-      }
-    }
-  }
+
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
+  var score = 0;
+  let scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+  score += 10;
+  scoreText.setText('Score: ' + score);
+
 }
 
 export default class ParallaxScene extends Phaser.Scene {
@@ -58,40 +53,71 @@ export default class ParallaxScene extends Phaser.Scene {
     });
   }
 
+  backgroundRepeat(scene, w, h, text, speed, s1, s2, o1, o2, player) {
+    const count = 101 * speed
+    let screenWidth = 0;
+    const platforms = scene.physics.add.staticGroup();
+
+    for (let i = 0; i < count; i++) {
+      let repeatImage = platforms.create((w + screenWidth), h, text).setOrigin(o1, o2).setScrollFactor(speed).setScale(s1, s2)
+      
+      if (text === "ground2") {
+        const coin = scene.physics.add.staticGroup({
+          key: 'star',
+          repeat: 5,
+          setXY: { x: w + screenWidth, y: 700 * 0.5, stepX: 150 },
+          setScale: { x: 0.4, y: 0.4 }
+        })
+        scene.physics.add.overlap(player, coin, collectStar, null, this)
+        scene.physics.add.collider(player, [repeatImage, coin]);
+      }
+      screenWidth += scene.scale.width
+    }
+
+
+  }
+
   create() {
     const height = this.scale.height
     const width = this.scale.width
     
     this.add.image(width * 0.5, height * 0.3 , 'sky')
     .setScrollFactor(0).setScale(0.8, 0.7)
-
-    this.cloud1 = backgroundRepeat(this, 0, height * 0.45,'cloud1', 0.07, 0.5, 0.5, 0, 1)
-    this.mountain = backgroundRepeat(this, 0, height,'mountain', 0.25, 0.5, 0.5, 0, 1)
-    this.cloud2 = backgroundRepeat(this, width/ 2, height * 0.5,'cloud2', 0.15, 0.5, 0.5, 0, 1)
-    this.grass2 = backgroundRepeat(this, width / 2.4, height / 1.5,'grass2', 0.5, 0.4, 0.4) 
-    this.grass1 = backgroundRepeat(this, width / 7.5, height / 1.5,'grass3', 0.5, 0.4, 0.4) 
-    this.grass3 = backgroundRepeat(this, width / 1.3, height / 1.5,'grass1', 0.5, 0.4, 0.4)  
-    this.ground = backgroundRepeat(this, 0, height / 1.1,'ground', 0.75, 0.5, 0.5, 0, 1) 
-
-    this.tree1 = backgroundRepeat(this, width / 5, height / 1.8,'tree', 0.75, 0.5, 0.5)
-    this.tree2 = backgroundRepeat(this, width / 1.3, height / 1.6,'tree', 0.75, 0.35, 0.35) 
-    this.rock1 = backgroundRepeat(this, width / 1.8, height / 1.3,'rock2', 0.75, 0.4, 0.4) 
-    this.rock2 = backgroundRepeat(this, width / 3.5, height / 1.3,'rock3', 0.75, 0.4, 0.4) 
-    this.rock3 = backgroundRepeat(this, width / 1.1, height / 1.3,'rock1', 0.75, 0.4, 0.4) 
-    this.flower2 = backgroundRepeat(this, width / 2.5, height / 1.3,'flower2', 0.75, 0.4, 0.4) 
+    
+    this.cloud1 = this.backgroundRepeat(this, 0, height * 0.45,'cloud1', 0.07, 0.5, 0.5, 0, 1)
+    this.mountain = this.backgroundRepeat(this, 0, height,'mountain', 0.25, 0.5, 0.5, 0, 1)
+    this.cloud2 = this.backgroundRepeat(this, width/ 2, height * 0.5,'cloud2', 0.15, 0.5, 0.5, 0, 1)
+    this.grass2 = this.backgroundRepeat(this, width / 2.4, height / 1.5,'grass2', 0.5, 0.4, 0.4) 
+    this.grass1 = this.backgroundRepeat(this, width / 7.5, height / 1.5,'grass3', 0.5, 0.4, 0.4) 
+    this.grass3 = this.backgroundRepeat(this, width / 1.3, height / 1.5,'grass1', 0.5, 0.4, 0.4)  
+    this.ground = this.backgroundRepeat(this, 0, height / 1.1,'ground', 0.75, 0.5, 0.5, 0, 1) 
+    
+    this.tree1 = this.backgroundRepeat(this, width / 5, height / 1.8,'tree', 0.75, 0.5, 0.5)
+    this.tree2 = this.backgroundRepeat(this, width / 1.3, height / 1.6,'tree', 0.75, 0.35, 0.35) 
+    this.rock1 = this.backgroundRepeat(this, width / 1.8, height / 1.3,'rock2', 0.75, 0.4, 0.4) 
+    this.rock2 = this.backgroundRepeat(this, width / 3.5, height / 1.3,'rock3', 0.75, 0.4, 0.4) 
+    this.rock3 = this.backgroundRepeat(this, width / 1.1, height / 1.3,'rock1', 0.75, 0.4, 0.4) 
+    this.flower2 = this.backgroundRepeat(this, width / 2.5, height / 1.3,'flower2', 0.75, 0.4, 0.4) 
     this.player = this.physics.add.sprite(width * 0.1, height * 0.4, 'player', 3).setScale(1.3, 1.3);
     this.player.setBounce(0.2);
-    this.flower1 = backgroundRepeat(this, width / 1.7, height / 1.2,'flower1', 0.75, 0.4, 0.4)
+    this.flower1 = this.backgroundRepeat(this, width / 1.7, height / 1.2,'flower1', 0.75, 0.4, 0.4)
     
     this.stars = this.physics.add.group({
       key: 'star',
-      repeat: 5,
-      setXY: { x: width * 0.5, y: width * 0.5, stepX: 70 }
+      repeat: 4,
+      setXY: { x: width * 0.5, y: width * 0.5, stepX: 70 },
+      setScale: { x: 0.4, y: 0.4 }
     });
-
-    this.ground2 = backgroundRepeat(this, 0, height,'ground2', 1.25, 0.45, 0.45, 0, 1 , this.player)
-    this.ground2 = backgroundRepeat(this, width * 0.5, height * 0.7, this.stars, 1.25, 0.45, 0.45, 0, 1 , this.stars)
-    // this.physics.add.overlap(this.player, this.stars, null, this)
+    this.stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+    
+    this.backgroundRepeat(this, width * 0.5, height, 'ground2', 1.25, 0.45, 0.45, 0, 1 , this.stars)
+    this.backgroundRepeat(this, 0, height,'ground2', 1.25, 0.45, 0.45, 0, 1 , this.player)
+    // this.backgroundRepeat(this, width * 0.5, height * 0.7, 'star', 1.25, 0.45, 0.45, 0, 1, this.player)
+    
+    this.physics.add.collider(this.player, this.ground2, this.ground);
+    this.physics.add.overlap(this.player, this.stars, collectStar, null, this)
     
     if (!this.anims.get('walking')) {
       this.anims.create({
@@ -109,14 +135,13 @@ export default class ParallaxScene extends Phaser.Scene {
       this.anims.create({
         key: 'star',
         frames: this.anims.generateFrameNames('star', {
-          frames: [30, 31, 32, 33, 34, 35, 32, 33, 34, 35, 32, 33, 34, 35, 32, 33, 34, 35]
+          frames: [0,1,2,3,4,5,6,7]
         }),
         frameRate: 12,
         yoyo: true,
         repeat: -1
       });
     }
-    
     
     this.cursors = this.input.keyboard.createCursorKeys();
     this.cameras.main.setBounds(0,0, width * 100 ,height)
@@ -141,6 +166,10 @@ export default class ParallaxScene extends Phaser.Scene {
       if (onGround)
       this.player.setFrame(10);
     }
+
+    this.stars.setVelocityX(0)
+    // this.stars.setVelocityY(0)
+    // this.stars.anims.play(star)
 
     if (onGround && (this.cursors.space.isDown || this.cursors.up.isDown)) {
       this.player.anims.stop('walking');
